1e894e9a241c77df2ae10a6f78b1f7894fc78e79
jaekyu-sim <worb1605@naver.com>
2020-11-29 11:16:06 +0900

    test

---
+++ src/main/java/shop/Cancellation.java
@@ -0,0 +1,44 @@
+package shop;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Cancellation_table")
+public class Cancellation {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long orderId;
+
+    @PrePersist
+    public void onPrePersist(){
+        DeliveryCanceled deliveryCanceled = new DeliveryCanceled();
+        BeanUtils.copyProperties(this, deliveryCanceled);
+        deliveryCanceled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+
+
+
+}
---
+++ src/main/java/shop/DeliveryCanceled.java
@@ -0,0 +1,26 @@
+package shop;
+
+public class DeliveryCanceled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+
+    public DeliveryCanceled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+}
